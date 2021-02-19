e5e8d1e31bad5ecbafeda47880542a514116b4ba
tamzidpeace <tamjedpeace@gmail.com>
2021-02-19 19:23:27 +0600

    vue componant

--- vue_01/vue.js
+++ vue_01/vue.js
@@ -1,35 +1,25 @@
 Vue.component('todo-item', {
-    props: ['todo'],
-    template: `<li>{{ todo.text }}</li>`
+    template: `<p>My name is {{name}} <button v-on:click="changeName">Change Name</button></p>`,
+    data: function () {
+        return {
+         name: 'Arafat',
+        }
+    },
+    methods: {
+      changeName: function () {
+          this.name = 'Kamal'
+      }
+    },
 })
 
-Vue.component('component1', {
-    template: `<div class="mb-3">
-  <label for="exampleFormControlInput1" class="form-label">Email address</label>
-  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
-</div>
-<div class="mb-3">
-  <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
-  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-</div>`
-})
-
-var app2 = new Vue({
-    el: '#app-2',
+var app3 = new Vue({
+    el: '#app-3',
     data: {
-        message: 'You loaded this page on ' + new Date().toLocaleString(),
-        groceryList: [{
-            id: 0,
-            text: 'Vegetables'
-        },
-            {
-                id: 1,
-                text: 'Cheese'
-            },
-            {
-                id: 2,
-                text: 'Whatever else humans are supposed to eat'
-            }
-        ],
+        title: 'Test',
+    },
+    methods: {
+        printName: function () {
+            console.log('Arafat');
+        }
     }
 })
