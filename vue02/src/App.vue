c40b4a422beb059a06f08e80949a630d011ff0aa
Arafat Kamal <tamjedpeace@gmail.com>
2021-02-20 10:03:30 +0600

    nested global & local component v2

--- vue02/src/App.vue
+++ vue02/src/App.vue
@@ -2,12 +2,17 @@
   <div>
     <h1>{{ title }}</h1>
     <p>{{ greeting() }}</p>
+
     <br>
+
     <h3>Ninjas</h3>
     <ninjas></ninjas>
+
     <br>
+
     <h3>Games</h3>
     <games></games>
+
   </div>
 </template>
 
--- vue02/src/Games.vue
+++ vue02/src/Games.vue
@@ -1,12 +1,14 @@
 <template>
-  <ul>
-    <li v-for="game in games"> {{game}} </li>
-  </ul>
+  <div>
+    <ul>
+      <li v-for="game in games"> {{ game }}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
-  data () {
+  data() {
     return {
       games: ['Fifa', 'NFS', 'COD']
     }
