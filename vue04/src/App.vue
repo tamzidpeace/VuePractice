18a1a991619e19e4751c2737a2b9b44253d74471
Arafat Kamal <tamjedpeace@gmail.com>
2021-03-07 20:30:15 +0600

    vue slots

--- vue04/src/App.vue
+++ vue04/src/App.vue
@@ -1,6 +1,20 @@
 <template>
   <div>
-    <form-helper></form-helper>
+    <form-helper>
+      <div slot="form-header">
+        <h3>This is the title of the form </h3>
+        <p>information about slot</p>
+      </div>
+
+      <div slot="form-fields">
+        <input type="text" placeholder="name" required>
+        <input type="password" placeholder="password" required>
+      </div>
+
+      <div slot="form-controls">
+        <button v-on:click="handleSubmit">Submit</button>
+      </div>
+    </form-helper>
   </div>
 </template>
 
--- vue04/src/components/form_helper.vue
+++ vue04/src/components/form_helper.vue
@@ -1,6 +1,28 @@
 <template>
   <div>
-    <h1>I am the form helper</h1>
+    <h1>Please fill out the form</h1>
+    <form action="">
+      <div id="form-header">
+        <slot name="form-header"></slot>
+      </div>
+
+      <div id="form-fields">
+        <slot name="form-fields"></slot>
+      </div>
+
+      <div id="form-control">
+        <slot name="form-controls"></slot>
+      </div>
+
+      <div id="useful-links">
+        <ul>
+          <li><a href="javascript:;">link 1</a></li>
+          <li><a href="javascript:;">link 2</a></li>
+          <li><a href="javascript:;">link 3</a></li>
+          <li><a href="javascript:;">link 4</a></li>
+        </ul>
+      </div>
+    </form>
   </div>
 </template>
 
