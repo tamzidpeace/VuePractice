1a5cf978b065c0b283f674ec230a2f5ccb20b95d
Arafat Kamal <tamjedpeace@gmail.com>
2021-03-07 20:57:19 +0600

    dynamic component

--- vue04/src/App.vue
+++ vue04/src/App.vue
@@ -1,33 +1,25 @@
 <template>
   <div>
-    <form-helper>
-      <div slot="form-header">
-        <h3>This is the title of the form </h3>
-        <p>information about slot</p>
-      </div>
-
-      <div slot="form-fields">
-        <input type="text" placeholder="name" required>
-        <input type="password" placeholder="password" required>
-      </div>
-
-      <div slot="form-controls">
-        <button v-on:click="handleSubmit">Submit</button>
-      </div>
-    </form-helper>
+    <keep-alive>
+      <component v-bind:is="component"></component>
+    </keep-alive>
+    <button v-on:click="component='form-one'">Show Form One</button>
+    <button v-on:click="component='form-two'">Show Form Two</button>
   </div>
 </template>
 
 <script>
-import formHelper from './components/form_helper'
+import formOne from './components/formOne'
+import formTwo from './components/formTwo'
 
 export default {
   components: {
-    'form-helper': formHelper
+    'form-one': formOne,
+    'form-two': formTwo,
   },
   data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      component: 'form-x'
     }
   },
   methods: {},
