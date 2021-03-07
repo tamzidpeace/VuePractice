3abb8576b7a76e512c98e58245a548aa00edb0f2
Arafat Kamal <tamjedpeace@gmail.com>
2021-03-07 19:52:43 +0600

    practicing slot

---
+++ vue04/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <form-helper></form-helper>
+  </div>
+</template>
+
+<script>
+import formHelper from './components/form_helper'
+
+export default {
+  components: {
+    'form-helper': formHelper
+  },
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods: {},
+}
+</script>
+
---
+++ vue04/src/components/form_helper.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>I am the form helper</h1>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  components: {},
+  data() {
+    return {}
+  },
+  methods: {}
+
+}
+
+</script>
