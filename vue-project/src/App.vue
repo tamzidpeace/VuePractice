50a493da6d17258163db9a1e8abeb1ff72f2832a
tamzidpeace <tamjedpeace@gmail.com>
2023-05-05 12:43:45 +0600

    ---

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,35 +1,13 @@
 <script>
-// give each todo a unique id
-let id = 0;
+import { useCounterStore } from '@/stores/counter'
 
-export default {
-  data() {
-    return {
-      newTodo: "",
-      todos: [{ id: id++, text: "Learn Vue" }],
-    };
-  },
-  methods: {
-    addTodo() {
-      // ...
-      this.todos.push({ id: id++, text: this.newTodo });
-    },
-    removeTodo(todo) {
-      this.todos.splice(this.todos.indexOf(todo), 1);
-    },
-  },
-};
+
+const counter = useCounterStore()
+
 </script>
 
 <template>
-  <form @submit.prevent="addTodo">
-    <input v-model="newTodo">
-    <button>Add Todo</button>
-  </form>
-  <ul>
-    <li v-for="todo in todos" :key="todo.id">
-      {{ todo.text }}
-      <button @click="removeTodo(todo)">X</button>
-    </li>
-  </ul>
+  <div>
+    <h1>{{ counter.count }}</h1>
+  </div>
 </template>
--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -7,8 +7,8 @@ import router from "./router";
 import "./assets/main.css";
 
 const app = createApp(App);
+const pinia = createPinia();
 
-app.use(createPinia());
+app.use(pinia);
 app.use(router);
-
 app.mount("#app");
