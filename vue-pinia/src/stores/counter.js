36bc0519bf184146fd4a8578b9b8bf972823c3cd
tamzidpeace <tamjedpeace@gmail.com>
2023-05-05 19:04:53 +0600

    working on vue learning

--- vue-pinia/src/stores/counter.js
+++ vue-pinia/src/stores/counter.js
@@ -1,12 +1,18 @@
-import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
-  }
-
-  return { count, doubleCount, increment }
+export const useCounterStore = defineStore('counter', {
+  state: () => ({
+    count: 1011,
+  }),
+  actions: {
+    increment() {
+      this.count++
+    },
+    decrement() {
+      this.count--
+    },
+    randomizeCounter() {
+      this.count = Math.round(100 * Math.random())
+    },
+  },
 })
--- vue-pinia/src/views/HomeView.vue
+++ vue-pinia/src/views/HomeView.vue
@@ -1,9 +1,17 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <h1>{{ count }}</h1>
+    <button @click="decrement">-</button>
+    <button @click="increment">+</button>
+  </div>
 </template>
+
+
+<script setup>
+import { useCounterStore } from '@/stores/counter';
+import { storeToRefs } from 'pinia'
+
+const store = useCounterStore();
+const { count, } = storeToRefs(store);
+const { increment, decrement } = store;
+</script>
