a34972081867d7ca45e60eb1fd99207e2114bf92
tamzidpeace <tamjedpeace@gmail.com>
2023-07-06 16:33:05 +0600

    ---

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -3,22 +3,28 @@ import { useTaskStore } from '../stores/taskStore'
 import { storeToRefs } from 'pinia'
 import Task from './TaskComponent.vue'
 import Form from './FormView.vue';
+import { ref } from 'vue';
+import MultiSelect from 'primevue/multiselect';
+
 
 
 const store = useTaskStore()
 const { task, tasks } = storeToRefs(store)
 const { addTask } = store
+
+const selectedCities = ref();
+const cities = ref([
+  { name: 'New York', code: 'NY' },
+  
+
+]);
 </script>
 
 <template>
   <div class="body">
     <div class="flex flex-row space-x-4 justify-center pt-10">
-      <input
-        class="rounded border-none border-[0px] focus:outline-none color-[#ccc]"
-        type="text"
-        :value="task"
-        @input="task = $event.target.value"
-      />
+      <input class="rounded border-none border-[0px] focus:outline-none color-[#ccc]" type="text" :value="task"
+        @input="task = $event.target.value" />
 
       <button class="bg-blue-500 text-white rounded px-2 py-1" @click="addTask">Add</button>
     </div>
@@ -31,13 +37,15 @@ const { addTask } = store
       </ul>
     </div>
 
-    -----------
+    <div class="card flex justify-content-center">
+      <MultiSelect v-model="selectedCities" :options="cities" filter optionLabel="name" placeholder="Select Cities"
+        :maxSelectedLabels="3" class="w-full md:w-20rem" />
+    </div>
+
     <div>
       <Form />
     </div>
   </div>
-
-  
 </template>
 
 
@@ -49,6 +57,7 @@ const { addTask } = store
   margin: 0%;
   padding: 0%;
 }
+
 .add-task {
   display: flex;
   direction: row;
